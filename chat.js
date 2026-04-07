cd0a95ef7318d3578c3cc8ac7299875be20688ca
JM20Solutions <gerard.picazo.es@gmail.com>
2026-04-07 15:48:32 +0200

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,5 +1,5 @@
 // ─── CONFIG ─────────────────────────────────────────────────────────────────
-const N8N_WEBHOOK_URL = 'https://gpixie.app.n8n.cloud/webhook-test/3c0cbaf5-6283-4ec7-b806-b1bca58b7852';
+const N8N_WEBHOOK_URL = 'https://gpixie.app.n8n.cloud/webhook/3c0cbaf5-6283-4ec7-b806-b1bca58b7852';
 // ────────────────────────────────────────────────────────────────────────────
 
 const widget      = document.getElementById('chatWidget');
